6c1509db36db25aa90f1ed0d7094113bd843f089
Bagabandi <76275588+Bagabandi152@users.noreply.github.com>
2022-09-20 14:46:57 +0800

    lab03 update

--- Labs/jtlab03/jtlab03/src/MainApp.java
+++ Labs/jtlab03/jtlab03/src/MainApp.java
@@ -3,13 +3,38 @@ import java.util.Scanner;
 
 public class MainApp {
 
-    MainApp(){
-        System.out.println("#Problem01: ");
-        testTriangleClass();
+    public static Scanner input(String txt){
+        System.out.print(txt);
+        return new Scanner(System.in);
+    }
+
+    MainApp(int probNum){
+        switch (probNum){
+            case 1: {
+                System.out.println("#Problem01: test Triangle class.");
+                testTriangleClass();
+                break;
+            }
+            case 2: {
+                System.out.println("#Problem02: sum the areas of geometric objects.");
+                sumAreasGeoObj();
+                break;
+            }
+            default:{
+                System.out.println("Sorry, problem not found!!!");
+                break;
+            }
+        }
     }
 
     public static void main(String[] args) {
-        new MainApp();
+        while (true){
+            int probNum = input("\nEnter problem number (if stop app, enter 0): ").nextInt();
+            if(probNum == 0){
+                break;
+            }
+            new MainApp(probNum);
+        }
     }
 
     public abstract class GeometricObject{
@@ -132,9 +157,98 @@ public class MainApp {
         }
     }
 
-    public Scanner input(String txt){
-        System.out.print(txt);
-        return new Scanner(System.in);
+    static final double pi = 3.14;
+    public class Circle extends GeometricObject{
+        double radius;
+
+        Circle(){
+            this(1);
+        }
+
+        Circle(double radius){
+            this.radius = radius;
+        }
+
+        public double getRadius() {
+            return radius;
+        }
+
+        public void setRadius(double radius) {
+            this.radius = radius;
+        }
+
+        @Override
+        public double getArea() {
+            double p = this.getPerimeter()/2;
+            return p*getRadius();
+        }
+
+        @Override
+        public double getPerimeter() {
+            return 2*getRadius()*pi;
+        }
+
+        @Override
+        public String toString() {
+            return "Circle{" +
+                    "radius=" + getRadius() +
+                    ", color='" + getColor() + '\'' +
+                    ", filled=" + isFilled() +
+                    ", area=" + getArea() +
+                    ", perimeter=" + getPerimeter() +
+                    '}';
+        }
+    }
+
+    public class Rectangle extends GeometricObject{
+        double a, b;
+
+        Rectangle(){
+            this(1, 1);
+        }
+
+        Rectangle(double a, double b){
+            this.a = a;
+            this.b = b;
+        }
+
+        public double getA() {
+            return a;
+        }
+
+        public void setA(double a) {
+            this.a = a;
+        }
+
+        public double getB() {
+            return b;
+        }
+
+        public void setB(double b) {
+            this.b = b;
+        }
+
+        @Override
+        public double getArea() {
+            return a*b;
+        }
+
+        @Override
+        public double getPerimeter() {
+            return a + b;
+        }
+
+        @Override
+        public String toString() {
+            return "Rectangle{" +
+                    "a=" + getA() +
+                    ", b=" + getB() +
+                    ", color='" + getColor() + '\'' +
+                    ", filled=" + isFilled() +
+                    ", area=" + getArea() +
+                    ", perimeter=" + getPerimeter() +
+                    '}';
+        }
     }
 
     //Lab02 ---> 13.01
@@ -147,5 +261,33 @@ public class MainApp {
         triangle.setFilled(input("Enter whether was filled: ").nextBoolean());
         System.out.println(triangle);
         System.out.println(triangle.isTriangle() ? "This triangle exist." : "This triangle not exist.");
+    }
+
+    //Lab02 ---> 13.12
+    public void sumAreasGeoObj(){
+        System.out.println("Triangle 1: ");
+        GeometricObject tri1 = new Triangle(input("Enter first a side: ").nextDouble(), input("Enter second a side: ").nextDouble(), input("Enter third a side: ").nextDouble());
+        System.out.println("Triangle2: ");
+        GeometricObject tri2 = new Triangle(input("Enter first a side: ").nextDouble(), input("Enter second a side: ").nextDouble(), input("Enter third a side: ").nextDouble());
+        System.out.println("Circle: ");
+        GeometricObject circle = new Circle(input("Enter a radius: ").nextDouble());
+        System.out.println("Rectangle: ");
+        GeometricObject rect = new Rectangle(input("Enter first a side: ").nextDouble(), input("Enter second a side: ").nextDouble());
+
+        GeometricObject[] geoArr = new GeometricObject[4];
+        geoArr[0] = tri1;
+        geoArr[1] = tri2;
+        geoArr[2] = circle;
+        geoArr[3] = rect;
+
+        System.out.printf("Geometric object area sum: %.3f", sumArea(geoArr));
+    }
+
+    public static double sumArea(GeometricObject[] a){
+        double sum = 0;
+        for(int i = 0; i < a.length; i++){
+            sum += a[i].getArea();
+        }
+        return sum;
     }
 }
