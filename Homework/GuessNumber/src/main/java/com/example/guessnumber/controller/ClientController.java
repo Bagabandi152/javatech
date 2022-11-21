997d7183e89d39cb6a1b8c461565bd7f5fb89165
Bagabandi <76275588+Bagabandi152@users.noreply.github.com>
2022-11-21 19:58:33 +0800

    update

--- Homework/GuessNumber/src/main/java/com/example/guessnumber/controller/ClientController.java
+++ Homework/GuessNumber/src/main/java/com/example/guessnumber/controller/ClientController.java
@@ -3,6 +3,7 @@ package com.example.guessnumber.controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.text.Text;
@@ -39,25 +40,61 @@ public class ClientController implements Initializable {
 
     @FXML
     void checkGuess(ActionEvent event) {
-        if (Integer.parseInt(guess.getText()) == randomNumber) {
-            downArrow.setVisible(false);
-            upArrow.setVisible(false);
-            correct.setVisible(true);
-        } else if (Integer.parseInt(guess.getText()) > randomNumber) {
-            downArrow.setVisible(true);
-            upArrow.setVisible(false);
-            correct.setVisible(false);
-        } else if (Integer.parseInt(guess.getText()) < randomNumber) {
-            downArrow.setVisible(false);
-            upArrow.setVisible(true);
-            correct.setVisible(false);
+        if (isNumeric(guess.getText())) {
+            if (Integer.parseInt(guess.getText()) == randomNumber) {
+                downArrow.setVisible(false);
+                upArrow.setVisible(false);
+                correct.setVisible(true);
+            } else if (Integer.parseInt(guess.getText()) > randomNumber) {
+                downArrow.setVisible(true);
+                upArrow.setVisible(false);
+                correct.setVisible(false);
+            } else if (Integer.parseInt(guess.getText()) < randomNumber) {
+                downArrow.setVisible(false);
+                upArrow.setVisible(true);
+                correct.setVisible(false);
+            }
+            guessCount++;
+            guessCounterText.setText("Оролдлого: " + guessCount);
+
+            if (guessCount == 3) {
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle(null);
+                String s = "Таны оролдлогын тоо дууслаа.";
+                alert.setContentText(s);
+                alert.showAndWait();
+                resetPlayer();
+            }
+        } else {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle(null);
+            String s = "Зөвхөн бүхэл оруулах шаардлагатай.";
+            alert.setContentText(s);
+            alert.showAndWait();
+            resetPlayer();
         }
-        guessCount++;
-        guessCounterText.setText("Оролдлого: " + guessCount);
+    }
+
+    public static boolean isNumeric(String string) {
+        System.out.println(String.format("Parsing string: \"%s\"", string));
+        if (string == null || string.equals("")) {
+            return false;
+        }
+        try {
+            int intValue = Integer.parseInt(string);
+            return true;
+        } catch (NumberFormatException ex) {
+            ex.fillInStackTrace();
+        }
+        return false;
     }
 
     @FXML
     void reset(ActionEvent event) {
+        resetPlayer();
+    }
+
+    void resetPlayer() {
         randomNumber = random.nextInt(100);
         downArrow.setVisible(false);
         upArrow.setVisible(false);
