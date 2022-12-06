cfb8e4995fe577cff4012edd24ffa6f02c130eb2
Bagabandi <76275588+Bagabandi152@users.noreply.github.com>
2022-12-06 13:30:32 +0800

    update

---
+++ Labs/jtlab14/src/test/AnalyzeNumbers.java
@@ -0,0 +1,24 @@
+package test;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class AnalyzeNumbers {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the number of items: ");
+        int n = input.nextInt();
+        double[] numbers = new double[n];
+
+        System.out.print("Enter the numbers: ");
+        for (int i = 0; i < n; i++) {
+            numbers[i] = input.nextDouble();
+        }
+
+        double average = Arrays.stream(numbers).average().getAsDouble();
+        int count = (int) Arrays.stream(numbers).filter(num -> num > average).count();
+
+        System.out.println("Average is " + average);
+        System.out.println("Number of elements above the average is " + count);
+    }
+}
