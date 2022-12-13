b8450a1d2fa46233da8de0356ae3f1d8606430b5
Bagabandi <76275588+Bagabandi152@users.noreply.github.com>
2022-12-13 14:10:12 +0800

    update lab15

---
+++ Labs/jtlab15/src/DisplayPrimeNumber.java
@@ -0,0 +1,54 @@
+import java.net.Inet4Address;
+import java.util.ArrayList;
+import java.util.Optional;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class DisplayPrimeNumber {
+
+    Scanner input;
+
+    DisplayPrimeNumber() {
+        System.out.println("Enter length of numbers: ");
+        input = new Scanner(System.in);
+        Integer len = input.nextInt();
+        ArrayList<Integer> primes = new ArrayList<>(len);
+
+        System.out.print("\nEnter numbers: ");
+        for (int i = 0; i < len; i++) {
+            primes.add(input.nextInt());
+        }
+
+        System.out.println("Entered prime numbers: ");
+        printArr((ArrayList<Integer>) primes.stream().filter(elm -> isPrime(elm).isPresent()).collect(Collectors.toList()));
+    }
+
+    Optional<Integer> isPrime(Integer num) {
+        Integer check, s = 0;
+        if (num == 1) {
+            check = null;
+        } else if (num == 2) {
+            check = num;
+        } else {
+            for (int i = 3; i <= Math.floor(Math.sqrt(num)); i++) {
+                if (num % i == 0) {
+                    s++;
+                }
+            }
+            if (s == 0) {
+                check = num;
+            } else {
+                check = null;
+            }
+        }
+        return Optional.ofNullable(check);
+    }
+
+    void printArr(ArrayList<Integer> arrayList) {
+        arrayList.stream().forEach(elm -> System.out.print(elm + " "));
+    }
+
+    public static void main(String[] args) {
+        new DisplayPrimeNumber();
+    }
+}
